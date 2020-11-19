bda3835b0f10e3b5d9d5a39cc3f26c16396562bf
shaoling <shaoling@pinduoduo.com>
2020-11-19 16:35:04 +0800

    feat: 引入compose.ts

---
+++ utils/compose.ts
@@ -0,0 +1,13 @@
+/**
+ * 组合函数
+ * @param funcs {Function}
+ */
+export default function compose(...funcs) {
+  if (funcs.length === 0) {
+    return (arg) => arg
+  }
+  if (funcs.length === 1) {
+    return funcs[0]
+  }
+  return funcs.reduce((a, b) => (...args) => a(b(...args)))
+}
